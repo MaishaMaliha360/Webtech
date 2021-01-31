38c5f3030ea4f5dfe5dc50608c9b99ba780edb49
Maisha Maliha <69841337+MaishaMaliha360@users.noreply.github.com>
2021-01-31 17:34:21 +0600

    lab task 1

---
+++ registration.php
@@ -0,0 +1,42 @@
+<DOCTYPE html>
+<html>
+<head>
+<title> Registration Page </title>
+</head>
+<body>
+<h1> Registration </h1>
+<form>
+<table>
+
+<tr><td>Name:</td>
+<td><input type="text" name="Name"></td></tr>
+<tr><td>Email:</td>
+<td><input type="text" name="Email"></td></tr>
+<tr><td>User Name:</td>
+<td><input type="text" name="User Name"></td></tr>
+
+<tr><td>Password:</td>
+<td><input type="Password" name="Password"></td></tr>
+
+<tr><td>Confirm Password:</td>
+<td><input type="text" name="Confirm Password"></td></tr>
+
+<tr><td>Gender:
+<input type="radio" id="male" name="gender" value="male">
+<lable for="male">Male</lable>
+<input type="radio" id="Female" name="gender" value="Female">
+<lable for="female">Female</lable><br>
+</td></tr>
+<tr><td>
+<lable for="birthday">Date of Birth: </lable>
+<input type="date" id="dob" name="dob">
+<br>
+<input type="submit" name="Submit">
+<input type="reset" id="dob" name="Reset">
+</tr></td>
+
+</table>
+</form>
+
+</body>
+</html>
